3fc0be501a993ed6e7ead6c126f7f13b3c935f18
Trent Rand <contact@trentrand.com>
2017-06-18 13:42:52 -0700

    Add build and run processes to gulpfile
    Instructions and explanation will soon be provided in README.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
+var fs = require('fs');
+
+// Gulp Plugins
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var shell = require('gulp-shell');
+var exec = require('gulp-exec');
 
 var homeDir = require('os').homedir();
 var scripts = [];
 
+var accessories = ['./homebridge-rf-outlet/'];
+
+var isProduction = (process.env.NODE_ENV === 'production');
+
 var config = {
+  example: './bridge/accessories/homebridge-rf-outlet/config-example.json',
   src: './bridge/config.json',
-  dest: homeDir + '/.homebridge/',
+  dest: homeDir + '/.homebridge/'
 };
+
+var accessories = {
+  src: ['./bridge/accessories/outlet.accessory.js'],
+  dest: homeDir + '/.homebridge/accessories/'
+}
+
 var bridgeConfig = require(config.src).bridge;
 
 gulp.task('clean', ['stop-homebridge'], function() {
@@ -23,9 +38,28 @@ gulp.task('copy-config', ['clean'], function() {
     .pipe(gulp.dest(config.dest));
 });
 
+gulp.task('build-accessories', ['clean'], function() {
+  var exec = require('child_process').exec;
+  process.chdir('./bridge/accessories/');
+  exec('gulp');
+  process.chdir('../../');
+});
+
+gulp.task('copy-accessories', ['clean'], function() {
+  return gulp.src(accessories.src)
+    .pipe(gulp.dest(accessories.dest));
+})
+
+gulp.task('debug-homebridge', ['copy-config'], function() {
+  gulp.src('')
+    .pipe(isProduction ? shell('homebridge') :
+      shell('DEBUG=* homebridge -D -P ./bridge/accessories/'));
+});
+
 gulp.task('start-homebridge', ['copy-config'], function() {
   gulp.src('')
-    .pipe(shell('homebridge'));
+    .pipe(isProduction ? shell('homebridge') :
+      shell('homebridge -P ./bridge/accessories/'));
 });
 
 gulp.task('stop-homebridge', function() {
@@ -38,4 +72,8 @@ gulp.task('watch', function() {
   gulp.watch(scripts, ['']);
 });
 
-gulp.task('default', ['stop-homebridge', 'copy-config', 'start-homebridge', 'watch']);
+gulp.task('build', ['stop-homebridge', 'copy-config', 'build-accessories', 'copy-accessories']);
+
+gulp.task('debug', ['debug-homebridge', 'watch']);
+
+gulp.task('run', ['start-homebridge', 'watch']);
