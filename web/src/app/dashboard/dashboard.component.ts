21d34ad553668401298f95df3a55cfdb936beb5b
Trent Rand <contact@trentrand.com>
2017-07-31 01:52:41 -0700

    Add title to dashboard parent template

--- web/src/app/dashboard/dashboard.component.ts
+++ web/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
 
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  parentTitle: string = "dashboard";
+  childTitle: string = "overview";
 
   constructor(router: Router, route: ActivatedRoute) {
+    router.events.subscribe((val) => {
+      this.childTitle = route.firstChild.routeConfig.path
+    });
   }
 
   ngOnInit() {
